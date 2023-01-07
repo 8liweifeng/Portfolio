b5f129fe33f93025b323d526c43238bc754d604a
Liwei Feng <liweeiif@gmail.com>
2023-01-07 16:29:25 -0700

    day3

--- js/app.js
+++ js/app.js
@@ -3,9 +3,19 @@
 const header = document.querySelector("header");
 
 function stickyNavbar() {
-    console.log(window.pageYOffset)
-    header.classList.toggle("scrolled", window.pageYOffset>100);
+    
+    header.classList.toggle("scrolled", window.pageYOffset>0);
 
 }
 stickyNavbar();
 window.addEventListener("scroll",stickyNavbar);
+
+/* --------- Reveal Animation --------- */ // I do not know why it can not work
+// let sr = ScrollReveal({
+//     duration: 2500,
+//     distance: "60px",
+// });
+
+// sr.reveal(".showcase-info", {delay:600});
+// sr.reveal(".showcase-image", {origin: "top", delay: 700});
+
