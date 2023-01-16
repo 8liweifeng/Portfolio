7e2a36d176b0023d250f89c8f7c47a1bce553db3
Liwei Feng <liweeiif@gmail.com>
2023-01-16 16:31:36 -0700

    day5

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 //alert("Hello!");
 
 const header = document.querySelector("header");
+
 const first_skill = document.querySelector(".skill:first-child")
 const sk_counters = document.querySelectorAll(".counter span");
 const progress_bars = document.querySelectorAll(".skill svg circle");
 
+const ml_section = document.querySelector(".milestones");
+const ml_counters = document.querySelectorAll(".numbers span");
+
+
+const prt_section = document.querySelector('.portfolio');
+const zoom_icons = document.querySelectorAll(".zoom-icon");
+const modal_overlay = document.querySelector(".modal-overlay");
+const images = document.querySelectorAll(".image img");
+const prev_btn = document.querySelector(".prev-btn");
+const next_btn = document.querySelector(".next-btn");
+
 window.addEventListener("scroll", ()=> {
     if (!skillsPlayed) skillsCounter();
+    if (!mlPlayed) mlCounter();
 })
 
 
@@ -53,7 +66,7 @@ function updateCount(num, maxNum) {
         num.innerText=currentNum+1;
         setTimeout(()=> {
             updateCount(num,maxNum);
-        }, 12);
+        }, 25);
     }
 }
 
@@ -73,7 +86,80 @@ function skillsCounter() {
     })
     progress_bars.forEach(
         (p) => (p.style.animation = "progress 2s ease-in-out forwards")
-        );
-
-       
+        ); 
 }
+
+/* ------------------ Services Animation --------------------- */
+let mlPlayed = false;
+
+function mlCounter() {
+    let mlPlayed = true;
+    if (!hasReached(ml_section)) return;
+   
+    ml_counters.forEach((counter,i)=> { 
+      let target = counter.dataset.target;
+      setTimeout(() => {
+        updateCount(counter,target);
+    },400);
+      
+});
+
+}
+
+
+/* ------------------ portfolio filter Animation --------------------- */
+var mixer = mixitup('.portfolio-gallery',{
+    selectors: {
+        target: '.prt-card'
+    },
+    animation: {
+        duration: 1500
+    }
+});
+
+/* ------------------ modal pop up Animation --------------------- */
+let currentIndex = 0;
+
+zoom_icons.forEach((icn,i) => icn.addEventListener("click",()=> {
+    prt_section.classList.add("open");
+    document.body.classList.add("stopScrolling");
+    currentIndex = i;
+    changeImage(currentIndex);
+}));
+
+modal_overlay.addEventListener("click", () => {
+    prt_section.classList.remove("open");
+    document.body.classList.remove("stopScrolling");
+})
+
+prev_btn.addEventListener("click", ()=> {
+    if (currentIndex===0) {
+        currentIndex=5;
+    } else{
+    currentIndex--;}
+    changeImage(currentIndex);
+});
+
+
+next_btn.addEventListener("click", ()=> {
+    if (currentIndex===5) {
+        currentIndex=0;
+    } else{
+    currentIndex++;}
+    
+    changeImage(currentIndex);
+});
+
+function changeImage(index) {
+    images.forEach(img => img.classList.remove("showImage"));
+    images[index].classList.add("showImage");
+
+}
+
+
+
+
+
+
+
+
